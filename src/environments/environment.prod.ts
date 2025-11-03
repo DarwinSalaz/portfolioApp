b8574c4db83ffde9fafe315b4ef3d64a6a05b8f0
DarwinSalaz <darwinsalaz@gmail.com>
2025-11-03 12:48:54 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   //url: 'http://inventory-dev.us-east-2.elasticbeanstalk.com'
   //url: 'http://localhost:5000'
   //url: 'https://p01--portfolio--9qjb75jfq5zn.code.run'
-  url: 'https://creative-code-darwinsalaz.koyeb.app'
+  url: 'https://creative-code-darwinsalaz.koyeb.app',
+  version: '2.1.6'  // Cambia esta versión cada vez que generes una nueva versión
 };
