966f7f3e7f60f18d5fd697d41311f7f23b8ba4cb
DarwinSalaz <darwinsalaz@gmail.com>
2025-11-12 22:35:54 -0500

    Update user.component.ts

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ItemUserCustom } from '../../interfaces/interfaces';
-import { NavController } from '@ionic/angular';
+import { NavController, ActionSheetController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,18 +13,62 @@ export class UserComponent implements OnInit {
   @Input() item: ItemUserCustom = {};
   @Input() redirectTo = '';
 
-  constructor(private navCtrl: NavController, public router: Router) { }
+  constructor(
+    private navCtrl: NavController, 
+    public router: Router,
+    private actionSheetCtrl: ActionSheetController
+  ) { }
 
   ngOnInit() {
     console.log(this.item.username);
   }
 
-  click() {
-    //this.navCtrl.navigateForward('/menu', )
-    //this.navCtrl.navigateRoot( '/menu', { animated: true } );
-    console.log('como fueee' + this.item.customer_id);
+  async openOptionsMenu(event: Event) {
+    event.stopPropagation();
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: this.item.main_text,
+      subHeader: this.item.second_text,
+      buttons: [
+        {
+          text: 'Ver Control de Caja',
+          icon: 'wallet',
+          handler: () => {
+            this.goToCashControl();
+          }
+        },
+        {
+          text: 'Reporte de Movimientos',
+          icon: 'document-text',
+          handler: () => {
+            this.openReport();
+          }
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close',
+          role: 'cancel'
+        }
+      ]
+    });
+
+    await actionSheet.present();
+  }
+
+  goToCashControl() {
+    console.log('Navegando a control de caja: ' + this.item.customer_id);
     this.router.navigate([this.redirectTo], {
-      queryParams: { username : this.item.username, customer_id : this.item.customer_id }
+      queryParams: { username: this.item.username, customer_id: this.item.customer_id }
+    });
+  }
+
+  openReport() {
+    this.router.navigate(['/user-movements-report'], {
+      queryParams: { 
+        username: this.item.username, 
+        application_user_id: this.item.application_user_id,
+        user_full_name: this.item.main_text
+      }
     });
   }
 
