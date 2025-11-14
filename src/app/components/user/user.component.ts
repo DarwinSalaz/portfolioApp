e1bc77b4f6a3ab91fe06fd48005c27b316bedaa7
DarwinSalaz <darwinsalaz@gmail.com>
2025-11-13 23:17:37 -0500

    Update user.component.ts

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ItemUserCustom } from '../../interfaces/interfaces';
-import { NavController, ActionSheetController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,59 +13,18 @@ export class UserComponent implements OnInit {
   @Input() item: ItemUserCustom = {};
   @Input() redirectTo = '';
 
-  constructor(
-    private navCtrl: NavController, 
-    public router: Router,
-    private actionSheetCtrl: ActionSheetController
-  ) { }
+  constructor(private navCtrl: NavController, public router: Router) { }
 
   ngOnInit() {
     console.log(this.item.username);
   }
 
-  async openOptionsMenu(event: Event) {
-    event.stopPropagation();
-
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: this.item.main_text,
-      subHeader: this.item.second_text,
-      buttons: [
-        {
-          text: 'Ver Control de Caja',
-          icon: 'wallet',
-          handler: () => {
-            this.goToCashControl();
-          }
-        },
-        {
-          text: 'Reporte de Movimientos',
-          icon: 'document-text',
-          handler: () => {
-            this.openReport();
-          }
-        },
-        {
-          text: 'Cancelar',
-          icon: 'close',
-          role: 'cancel'
-        }
-      ]
-    });
-
-    await actionSheet.present();
-  }
-
-  goToCashControl() {
-    console.log('Navegando a control de caja: ' + this.item.customer_id);
+  click() {
+    console.log('Navegando con application_user_id: ' + this.item.application_user_id);
     this.router.navigate([this.redirectTo], {
-      queryParams: { username: this.item.username, customer_id: this.item.customer_id }
-    });
-  }
-
-  openReport() {
-    this.router.navigate(['/user-movements-report'], {
       queryParams: { 
         username: this.item.username, 
+        customer_id: this.item.customer_id,
         application_user_id: this.item.application_user_id,
         user_full_name: this.item.main_text
       }
